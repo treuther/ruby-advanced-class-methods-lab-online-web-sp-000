4648c401f1d52ee1c1f655e22a2b1ea018c39418
Tracy Reuther <tracyreuther@gmail.com>
2019-06-24 08:20:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,42 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(string_name_of_the_song)
+    new_song = self.new
+    new_song.name = string_name_of_the_song
+    new_song
+  end
+
+  def self.create_by_name(string_name_of_the_song)
+    new_song = self.new
+    new_song.name = string_name_of_the_song
+    new_song.save
+    new_song
+  end
+
+  def self.find_by_name(string_name_of_the_song)
+    self.all.find do |title|
+      title.name == string_name_of_the_song
+    end
+  end
+
+  def self.find_or_create_by_name(string_name_of_the_song)
+    self.find_by_name(string_name_of_the_song) || self.create_by_name(string_name_of_the_song)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|x| x.name}
+  end
+
+  def self.new_from_filename(ext_mp3)
+    new_song = self.new
+    new_song.name =
+  end
+
 end
